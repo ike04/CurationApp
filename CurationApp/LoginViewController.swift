49ccae6e3c8f3b50ac39631b41386f9c8f560774
ike04 <boc.0409@outlook.jp>
2020-07-23 15:09:26 +0900

    ログイン画面の仮実装

--- CurationApp/LoginViewController.swift
+++ CurationApp/LoginViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField! {
+        didSet {
+            emailTextField.placeholder = "email"
+            emailTextField.clearButtonMode = .always
+        }
+    }
+    
+    @IBOutlet weak var passwordTextField: UITextField! {
+        didSet {
+            passwordTextField.placeholder = "password"
+            passwordTextField.clearButtonMode = .always
+        }
+    }
+    
+    @IBOutlet weak var loginButton: UIButton! {
+        didSet {
+            loginButton.setTitle("ログイン", for: UIControl.State.normal)
+            loginButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+            loginButton.backgroundColor = UIColor.systemBlue
+            loginButton.layer.cornerRadius = 10
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func didLoginButtonTapped(_ sender: Any) {
+        // TODO
+    }
+    
 }
-
