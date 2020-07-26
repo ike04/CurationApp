3f80810890ac9842635c1cfc9895824fc1f7bb35
ike04 <boc.0409@outlook.jp>
2020-07-26 22:55:48 +0900

    TabPageViewControllerの導入

---
+++ CurationApp/Controller/PageTabMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PageTabMenuViewController.swift
+//  CurationApp
+//
+//  Created by 池田一成 on 2020/07/26.
+//  Copyright © 2020 池田一成. All rights reserved.
+//
+
+import UIKit
+
+class PageTabMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
