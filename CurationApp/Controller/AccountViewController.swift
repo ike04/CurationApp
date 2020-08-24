afca0a904bd8f45b6479b020377ebb6f576ae3ea
ike04 <boc.0409@outlook.jp>
2020-08-24 11:18:30 +0900

    ラベルにテストデータを設定

--- CurationApp/Controller/AccountViewController.swift
+++ CurationApp/Controller/AccountViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 import TabPageViewController
 
 class AccountViewController: UIViewController {
+    private var users: [User] = [User]()
     
+    // MARK: -Propaties
     private lazy var logoutButton: UIButton = {
         let button = UIButton()
         button.addTarget(self, action: #selector(logoutButtonEvent(_:)), for: .touchUpInside)
@@ -20,7 +22,7 @@ class AccountViewController: UIViewController {
         button.layer.borderWidth = 1
         button.setTitleColor(UIColor.white, for: .normal)
         button.sizeToFit()
-        button.layer.cornerRadius = 10
+        button.layer.cornerRadius = 5
         button.backgroundColor = UIColor(red: 0, green: 122 / 255, blue: 1, alpha: 1)
         button.center = view.center
         return button
@@ -32,18 +34,37 @@ class AccountViewController: UIViewController {
         return label
     }()
     
+    private lazy var loginUserNameLabel: UILabel = {
+        let label = UILabel()
+        label.layer.borderWidth = 0.5
+        label.layer.cornerRadius = 5.0
+        label.layer.borderColor = UIColor.darkGray.cgColor
+        return label
+    }()
+    
     private lazy var emailLabel: UILabel = {
         let label = UILabel()
         label.text = R.string.localizable.email()
         return label
     }()
     
+    private lazy var loginEmailLabel: UILabel = {
+        let label = UILabel()
+        label.layer.borderWidth = 0.5
+        label.layer.cornerRadius = 5.0
+        label.layer.borderColor = UIColor.darkGray.cgColor
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // テスト用データ
+        users = [User(userName: "TestUser",
+                      email: "test@example.com")]
+        
         getNavigation()
         setupLayout()
-        // TODO
     }
     
     // MARK: - Navigation
@@ -51,23 +72,38 @@ class AccountViewController: UIViewController {
         self.navigationItem.title = R.string.localizable.account()
     }
     
+    // MARK: - Auto Layout
     private func setupLayout() {
-        [userNameLabel, emailLabel, logoutButton].forEach {
+        [userNameLabel, loginUserNameLabel, emailLabel, loginEmailLabel, logoutButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview($0)
         }
         
         // ユーザー名
-        userNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -50).isActive = true
+        userNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -20).isActive = true
         userNameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -250).isActive = true
         userNameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        userNameLabel.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        userNameLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.75).isActive = true
+        
+        // ログインユーザー名
+        loginUserNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -20).isActive = true
+        loginUserNameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -200).isActive = true
+        loginUserNameLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        loginUserNameLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.75).isActive = true
+        loginUserNameLabel.text = users[0].userName
         
         // メールアドレス
-        emailLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -50).isActive = true
+        emailLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -20).isActive = true
         emailLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -150).isActive = true
         emailLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        emailLabel.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        emailLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.75).isActive = true
+        
+        // ログインメールアドレス
+        loginEmailLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -20).isActive = true
+        loginEmailLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100).isActive = true
+        loginEmailLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        loginEmailLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.75).isActive = true
+        loginEmailLabel.text = users[0].email
         
         // ログアウト
         logoutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -75,7 +111,6 @@ class AccountViewController: UIViewController {
         logoutButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         logoutButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
         
-        // TODO
     }
     
     @objc private func logoutButtonEvent(_ sender: UIButton) {
